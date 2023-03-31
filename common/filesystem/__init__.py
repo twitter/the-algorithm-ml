94a7b6fd8e015f4c089f3acbe61d718a7245c08f
Luke <luke@nightcat.gg>
2023-03-31 19:43:51 -0400

    Remove all files
    This PR removes all files from the repository, as chronological order is objectively better.

--- common/filesystem/__init__.py
+++
@@ -1,1 +0,0 @@
-from tml.common.filesystem.util import infer_fs, is_gcs_fs, is_local_fs
--- projects/home/recap/model/__init__.py
+++
@@ -1,7 +0,0 @@
-from tml.projects.home.recap.model.entrypoint import (
-  create_ranking_model,
-  sanitize,
-  unsanitize,
-  MultiTaskRankingModel,
-)
-from tml.projects.home.recap.model.model_and_loss import ModelAndLoss
